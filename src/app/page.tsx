cdbc133039ac39436dfb2e864ca315ec0612b189
callioptsu <pasmac.dev@protonmail.com>
2024-07-30 16:00:08 -0300

    chore: version 0.1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import React from 'react'
+
+/**
+ * Development branch @version 0.1.0
+ * Component that renders a window with general features.
+ * @returns {JSX.Element} Home component.
+ */
+export default function Home(): JSX.Element {
   return (
     <main>
       <span>Home</span>
